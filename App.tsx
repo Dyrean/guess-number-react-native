35a2efcfb77a5a92ac432acd55aeb12d4723b3d8
Fatih Canbey <fcanbeyy@gmail.com>
2022-03-29 04:09:12 +0300

    navigation fix

--- App.tsx
+++ App.tsx
@@ -1,34 +1,19 @@
-import { StyleSheet, ImageBackground } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import GameScreen from './screens/GameScreen';
 import GameStartScreen from './screens/GameStartScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <LinearGradient colors={['orange', '#4e0429']} style={styles.rootScreen}>
-        <ImageBackground
-          source={require('./assets/images/dice-background.jpg')}
-          resizeMode="cover"
-          style={styles.rootScreen}
-          imageStyle={styles.backgroundImage}
-        >
-          <GameStartScreen />
-        </ImageBackground>
-      </LinearGradient>
-      <StatusBar style="light" />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="GameStartScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="GameStartScreen" component={GameStartScreen} />
+        <Stack.Screen name="GameScreen" component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  },
-  backgroundImage: {
-    opacity: 0.2,
-  },
-});
